7386024fa1e27411ed0f56c19292ca132ce3c5ac
Samuel Li <lisamuel1990@gmail.com>
2017-05-13 00:15:51 -0400

    init

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,10 @@
 var renderer = autoDetectRenderer(512, 512);
 
 //Add the canvas to the HTML document
-document.body.appendChild(renderer.view);
+//document.body.appendChild(renderer.view);
 
 //Create a container object called the `stage`
 var stage = new Container();
+
+//Init SpriteUtilities
+var u = new SpriteUtilities(PIXI);
